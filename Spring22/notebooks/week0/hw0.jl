c1e08ba90327a317aef901bd1ec851626bea1ebf
jdossgollin <jdossgollin@users.noreply.github.com>
2021-11-15 16:21:43 +0000

    Deploying to Spring22-franklin-output from  @ e7558a4c7475a98d7705c6ebaa0553c6511f6abf 🚀

--- Spring22/notebooks/week0/hw0.jl
+++ Spring22/notebooks/week0/hw0.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.20
+# v0.17.1
 
 using Markdown
 using InteractiveUtils
@@ -7,19 +7,42 @@ using InteractiveUtils
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
     quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
         local el = $(esc(element))
-        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
     end
 end
 
+# ╔═╡ 851c03a4-e7a4-11ea-1652-d59b7a6599f0
+# setting up an empty package environment
+begin
+    using Pkg: Pkg
+    Pkg.activate(mktempdir())
+    Pkg.Registry.update()
+end
+
+# ╔═╡ d6ee91ea-e750-11ea-1260-31ebf3ec6a9b
+# add (ie install) a package to our environment
+begin
+    Pkg.add("Compose")
+    # call `using` so that we can use it in our code
+    using Compose
+end
+
+# ╔═╡ 5acd58e0-e856-11ea-2d3d-8329889fe16f
+begin
+    Pkg.add("PlutoUI")
+    using PlutoUI
+end
+
 # ╔═╡ fafae38e-e852-11ea-1208-732b4744e4c2
-md"_Homework 0, version 3 -- Spring 2021_"
+md"_Homework 0, version 1 -- Fall 2022_"
 
 # ╔═╡ 7308bc54-e6cd-11ea-0eab-83f7535edf25
-# edit the code below to set your name and kerberos ID (i.e. email without @mit.edu)
+# edit the code below to set your name and net ID (i.e. email without rice.edu)
 
-student = (name = "Jazzy Doe", kerberos_id = "jazz")
+student = (name="Jazzy Doe", netid="jd123")
 
 # press the ▶ button in the bottom right of this cell to run your edits
 # or use Shift+Enter
@@ -30,29 +53,28 @@ student = (name = "Jazzy Doe", kerberos_id = "jazz")
 # ╔═╡ cdff6730-e785-11ea-2546-4969521b33a7
 md"""
 
-Submission by: **_$(student.name)_** ($(student.kerberos_id)@mit.edu)
+Submission by: **_$(student.name)_** ($(student.netid)@rice.edu)
 """
 
 # ╔═╡ a2181260-e6cd-11ea-2a69-8d9d31d1ef0e
 md"""
 # Homework 0: Getting up and running
 
-HW0 release date: Monday, Feb 15, 2021.
+HW0 release date: **TBD**
 
-**HW0 due date: Thursday, Feb 18, 2021, 11:59pm EST**, _but best completed before Wednesday's lecture if possible_.
+**HW0 due date: TBD**.
 
-First of all, **_welcome to the course!_** We are excited to teach you about real world applications of scientific computing, using the same tools that we work with ourselves.
+First of all, **_welcome to the course!_**
+I am excited to teach you about the exciting world of modeling and understanding environmental data.
 
-We'd like everyone to **submit this zeroth homework assignment**. It will not affect your grade, but it will help us get everything running smoothly when the course starts. If you're stuck or don't have much time, just fill in your name and ID and submit 🙂
+This homework assignment will help us get everything running smoothly when the course starts.
 """
 
 # ╔═╡ 31a8fbf8-e6ce-11ea-2c66-4b4d02b41995
 md"""## Homework Logistics
-Homeworks are in the form of [Pluto notebooks](https://github.com/fonsp/Pluto.jl). Your must complete them and submit them on [Canvas](https://canvas.mit.edu/courses/5637) (if you are an MIT student.). If you are not an MIT student, we encourage you to [join Discord](https://discord.gg/Z5qnVf8) and find someone to cross-grade.
+Homeworks are in the form of [Pluto notebooks](https://github.com/fonsp/Pluto.jl). Your must complete them and submit them on [Canvas](https://canvas.rice.edu/courses/48366) (if you are an enrolled Rice student.).
 
-Homeworks will be released on Thursdays and due on Thursdays 11:59pm Eastern time.
-
-HW0 is for you to get your system set up correctly and to test our grading software. You must submit it but it will not count towards your grade.
+HW0 is for you to get your system set up correctly and to test our grading software.
 """
 
 # ╔═╡ f9d7250a-706f-11eb-104d-3f07c59f7174
@@ -65,7 +87,7 @@ That’s it, but if you like you can do the _OPTIONAL_ exercises that follow."
 
 # ╔═╡ 430a260e-6cbb-11eb-34af-31366543c9dc
 md"""# Installation
-Before being able to run this notebook succesfully locally, you will need to [set up Julia and Pluto.](/Spring21/installation/)
+Before being able to run this notebook succesfully locally, you will need to [set up Julia and Pluto.](/Spring22/installation/)
 
 One you have Julia and Pluto installed, you can click the button at the top right of this page and follow the instructions to edit this notebook locally and submit.
 """
@@ -85,28 +107,28 @@ Output: $x^2$
 
 # ╔═╡ e02f7ea6-7024-11eb-3672-fd59a6cff79b
 function basic_square(x)
-	return 1 # this is wrong, write your code here!
+    return 1 # this is wrong, write your code here!
 end
 
 # ╔═╡ 6acef56c-7025-11eb-2524-819c30a75d39
 let
-	result = basic_square(5)
-	if !(result isa Number)
-		md"""
-!!! warning "Not a number"
-    `basic_square` did not return a number. Did you forget to write `return`?
-		"""
-	elseif abs(result - 5*5) < 0.01
-		md"""
-!!! correct
-    Well done!
-		"""
-	else
-		md"""
-!!! warning "Incorrect"
-    Keep working on it!
-		"""
-	end
+    result = basic_square(5)
+    if !(result isa Number)
+        md"""
+      !!! warning "Not a number"
+          `basic_square` did not return a number. Did you forget to write `return`?
+      		"""
+    elseif abs(result - 5 * 5) < 0.01
+        md"""
+      !!! correct
+          Well done!
+      		"""
+    else
+        md"""
+      !!! warning "Incorrect"
+          Keep working on it!
+      		"""
+    end
 end
 
 # ╔═╡ 348cea34-7025-11eb-3def-41bbc16c7512
@@ -115,11 +137,11 @@ md"That's all that's required for this week. Please submit the notebook. We just
 If you want to explore further, we have included a few optional exercises below."
 
 # ╔═╡ b3c7a050-e855-11ea-3a22-3f514da746a4
-if student.kerberos_id === "jazz"
-	md"""
-!!! danger "Oops!"
-    **Before you submit**, remember to fill in your name and kerberos ID at the top of this notebook!
-	"""
+if student.netid === "jazz"
+    md"""
+   !!! danger "Oops!"
+       **Before you submit**, remember to fill in your name and kerberos ID at the top of this notebook!
+   	"""
 end
 
 # ╔═╡ 339c2d5c-e6ce-11ea-32f9-714b3628909c
@@ -156,14 +178,14 @@ This is because the square root must be between the numbers `x/a` and `a`. Why?
 # ╔═╡ bccf0e88-e754-11ea-3ab8-0170c2d44628
 ex_1_1 = md"""
 your answer here
-""" 
+"""
 
 # you might need to wait until all other cells in this notebook have completed running. 
 # scroll down the page to see what's up
 
 # ╔═╡ e7abd366-e7a6-11ea-30d7-1b6194614d0a
 if !(@isdefined ex_1_1)
-	md"""Do not change the name of the variable - write you answer as `ex_1_1 = "..."`"""
+    md"""Do not change the name of the variable - write you answer as `ex_1_1 = "..."`"""
 end
 
 # ╔═╡ d62f223c-e754-11ea-2470-e72a605a9d7e
@@ -173,7 +195,7 @@ Write a function newton_sqrt(x) which implements the above algorithm."
 
 # ╔═╡ 4896bf0c-e754-11ea-19dc-1380bb356ab6
 function newton_sqrt(x, error_margin=0.01, a=x / 2) # a=x/2 is the default value of `a`
-	return x # this is wrong, write your code here!
+    return x # this is wrong, write your code here!
 end
 
 # ╔═╡ 7a01a508-e78a-11ea-11da-999d38785348
@@ -181,23 +203,23 @@ newton_sqrt(2)
 
 # ╔═╡ 682db9f8-e7b1-11ea-3949-6b683ca8b47b
 let
-	result = newton_sqrt(2, 0.01)
-	if !(result isa Number)
-		md"""
-!!! warning "Not a number"
-    `newton_sqrt` did not return a number. Did you forget to write `return`?
-		"""
-	elseif abs(result - sqrt(2)) < 0.01
-		md"""
-!!! correct
-    Well done!
-		"""
-	else
-		md"""
-!!! warning "Incorrect"
-    Keep working on it!
-		"""
-	end
+    result = newton_sqrt(2, 0.01)
+    if !(result isa Number)
+        md"""
+      !!! warning "Not a number"
+          `newton_sqrt` did not return a number. Did you forget to write `return`?
+      		"""
+    elseif abs(result - sqrt(2)) < 0.01
+        md"""
+      !!! correct
+          Well done!
+      		"""
+    else
+        md"""
+      !!! warning "Incorrect"
+          Keep working on it!
+      		"""
+    end
 end
 
 # ╔═╡ 088cc652-e7a8-11ea-0ca7-f744f6f3afdd
@@ -230,28 +252,6 @@ md"To draw Sierpinski's triangle, we are going to use an external package, [_Com
 A package contains a coherent set of functionality that you can often use a black box according to its specification. There are [lots of Julia packages](https://juliahub.com/ui/Home).
 "
 
-# ╔═╡ 851c03a4-e7a4-11ea-1652-d59b7a6599f0
-# setting up an empty package environment
-begin
-	import Pkg
-	Pkg.activate(mktempdir())
-	Pkg.Registry.update()
-end
-
-# ╔═╡ d6ee91ea-e750-11ea-1260-31ebf3ec6a9b
-# add (ie install) a package to our environment
-begin
-	Pkg.add("Compose")
-	# call `using` so that we can use it in our code
-	using Compose
-end
-
-# ╔═╡ 5acd58e0-e856-11ea-2d3d-8329889fe16f
-begin
-	Pkg.add("PlutoUI")
-	using PlutoUI
-end
-
 # ╔═╡ dbc4da6a-e7b4-11ea-3b70-6f2abfcab992
 md"Just like the definition above, our `sierpinksi` function is _recursive_: it calls itself."
 
@@ -259,16 +259,16 @@ md"Just like the definition above, our `sierpinksi` function is _recursive_: it 
 complexity = 3
 
 # ╔═╡ 1eb79812-e7b5-11ea-1c10-63b24803dd8a
-if complexity == 3 
-	md"""
-Try changing the value of **`complexity` to `5`** in the cell above. 
+if complexity == 3
+    md"""
+   Try changing the value of **`complexity` to `5`** in the cell above. 
 
-Hit `Shift+Enter` to affect the change.
-	"""
+   Hit `Shift+Enter` to affect the change.
+   	"""
 else
-	md"""
-**Great!** As you can see, all the cells in this notebook are linked together by the variables they define and use. Just like a spreadsheet!
-	"""
+    md"""
+   **Great!** As you can see, all the cells in this notebook are linked together by the variables they define and use. Just like a spreadsheet!
+   	"""
 end
 
 # ╔═╡ d7e8202c-e7b5-11ea-30d3-adcd6867d5f5
@@ -290,20 +290,20 @@ area_sierpinski(1) = 0.??
 
 # ╔═╡ ca8d2f72-e7b6-11ea-1893-f1e6d0a20dc7
 function area_sierpinski(n)
-	return 1.0
+    return 1.0
 end
 
 # ╔═╡ 71c78614-e7bc-11ea-0959-c7a91a10d481
 if area_sierpinski(0) == 1.0 && area_sierpinski(1) == 3 / 4
-	md"""
-!!! correct
-    Well done!
-	"""
+    md"""
+   !!! correct
+       Well done!
+   	"""
 else
-	md"""
-!!! warning "Incorrect"
-    Keep working on it!
-	"""
+    md"""
+   !!! warning "Incorrect"
+       Keep working on it!
+   	"""
 end
 
 # ╔═╡ c21096c0-e856-11ea-3dc5-a5b0cbf29335
@@ -329,24 +329,26 @@ triangle() = compose(context(), polygon([(1, 1), (0, 1), (1 / 2, 0)]))
 # program in. The best way to organize code is the one that promotes understanding.
 
 function place_in_3_corners(t)
-	# Uses the Compose library to place 3 copies of t
-	# in the 3 corners of a triangle.
-	# treat this function as a black box,
-	# or learn how it works from the Compose documentation here https://giovineitalia.github.io/Compose.jl/latest/tutorial/#Compose-is-declarative-1
-	compose(context(),
-			(context(1 / 4,   0, 1 / 2, 1 / 2), t),
-			(context(0, 1 / 2, 1 / 2, 1 / 2), t),
-			(context(1 / 2, 1 / 2, 1 / 2, 1 / 2), t))
+    # Uses the Compose library to place 3 copies of t
+    # in the 3 corners of a triangle.
+    # treat this function as a black box,
+    # or learn how it works from the Compose documentation here https://giovineitalia.github.io/Compose.jl/latest/tutorial/#Compose-is-declarative-1
+    return compose(
+        context(),
+        (context(1 / 4, 0, 1 / 2, 1 / 2), t),
+        (context(0, 1 / 2, 1 / 2, 1 / 2), t),
+        (context(1 / 2, 1 / 2, 1 / 2, 1 / 2), t),
+    )
 end
 
 # ╔═╡ e2848b9a-e703-11ea-24f9-b9131434a84b
 function sierpinski(n)
-	if n == 0
-		triangle()
-	else
-		t = sierpinski(n - 1) # recursively construct a smaller sierpinski's triangle
-		place_in_3_corners(t) # place it in the 3 corners of a triangle
-	end
+    if n == 0
+        triangle()
+    else
+        t = sierpinski(n - 1) # recursively construct a smaller sierpinski's triangle
+        place_in_3_corners(t) # place it in the 3 corners of a triangle
+    end
 end
 
 # ╔═╡ 9664ac52-e750-11ea-171c-e7d57741a68c
@@ -373,7 +375,7 @@ has area **$(area_sierpinski(n))**
 # ╟─a2181260-e6cd-11ea-2a69-8d9d31d1ef0e
 # ╟─31a8fbf8-e6ce-11ea-2c66-4b4d02b41995
 # ╟─f9d7250a-706f-11eb-104d-3f07c59f7174
-# ╟─430a260e-6cbb-11eb-34af-31366543c9dc
+# ╠═430a260e-6cbb-11eb-34af-31366543c9dc
 # ╟─a05d2bc8-7024-11eb-08cb-196543bbb8fd
 # ╠═e02f7ea6-7024-11eb-3672-fd59a6cff79b
 # ╟─6acef56c-7025-11eb-2524-819c30a75d39
