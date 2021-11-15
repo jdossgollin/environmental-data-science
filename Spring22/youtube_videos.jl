b732f647892f6e58c8740942b6e5a29b1a4c4190
jdossgollin <jdossgollin@users.noreply.github.com>
2021-11-15 17:12:46 +0000

    Deploying to Spring22-franklin-output from  @ abb4f48bb150e4243ad056b07839be0a4f7b5125 🚀

--- Spring22/youtube_videos.jl
+++ Spring22/youtube_videos.jl
@@ -1,4 +1,4 @@
 # This dictionary maps easy to remember names to Youtube video IDs
 # after adding an ID here, you can use the {{youtube <shortname>}}
 # syntax in your markdown files to embed the video into the page!
-videos = Dict("course-intro" => "vxjRWtWoD_w")
+videos = Dict("installation" => "OOjKEgbt8AI")
