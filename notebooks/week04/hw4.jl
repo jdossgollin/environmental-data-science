84fcc193d9536e6568683fa011c5d0f592576b6a
jdossgollin <jdossgollin@users.noreply.github.com>
2022-01-30 00:49:08 +0000

    Deploying to Spring22-deploy from  @ 8f868ce757b6dc689aa54bfbebe21453af48280c 🚀

---
+++ notebooks/week04/hw4.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.17.7
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b6a2da06-8165-11ec-1313-dbce9584cc38
+md"""
+# HW 4
+
+This is a placeholder
+"""
+
+# ╔═╡ Cell order:
+# ╠═b6a2da06-8165-11ec-1313-dbce9584cc38
